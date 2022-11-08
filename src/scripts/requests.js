58794d951ad6ae92d2e61a8e608baa6d7a9a4d02
TommiL90 <tomasbenjamin.l90@gmail.com>
2022-11-08 09:54:46 -0300

    create request login and request users

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -18,3 +18,127 @@ export async function registerUser(body) {
     console.log(err);
   }
 }
+
+async function requestLogin(body){
+
+  try{
+      const request = await fetch(baseUrl + "/session/login", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(body)
+      })  
+
+      if (request.ok){
+
+          const response = await request.json()
+          const {user:{name, email, avatar_url}, token} = response
+          
+          localStorage.setItem("@kezieAdopt:User", JSON.stringify(token))
+
+      }
+  }catch(err){
+      console.log(err)
+  }
+}
+
+async function requestCreateUser(body){
+
+  try{
+      const request = await fetch(baseUrl + "/users", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(body)
+      })  
+
+      if (request.ok){
+
+          const response = await request.json()
+          const {id, name, email, avatar_url} = response
+          
+
+      }
+  }catch(err){
+      console.log(err)
+  }
+}
+
+async function requestGetAllUsers(token){
+
+  try{
+      const request = await fetch(baseUrl + "/users", {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+
+        if(request.ok){
+          const response = await request.json()
+          console.log(response)
+          return response
+        }
+  }catch(err){
+          console.log(err)
+  }
+}
+
+async function requestReadProfile(token){
+
+  try{
+      const request = await fetch(baseUrl + "/users/profile", {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+
+      if(request.ok){
+          const response = await request.json()
+          return response            
+      }
+  }catch(err){
+          console.log(err)
+  }
+}
+
+async function requestUpdateProfile(token, body){
+
+  try{
+      const request = await fetch (baseUrl + "/users/profile", {
+          method: "PATCH",
+          headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`
+          },
+          body: JSON.stringify(body)
+      })
+
+      if (request.ok){
+
+          console.log("ok")
+      }
+  }catch(err){
+      console.log(err)
+  }
+}
+
+async function deleteProfile(token){
+
+  try{
+      const request = await fetch(baseUrl + "/users/profile", {
+          method: 'DELETE',
+          headers: { 
+            Authorization: `Bearer ${token}`
+          },
+      })
+  }catch(err){
+          console.log(err)
+  }
+}
+
+export {registerUser, requestLogin, requestCreateUser, requestGetAllUsers, requestReadProfile, requestUpdateProfile, deleteProfile}
+
