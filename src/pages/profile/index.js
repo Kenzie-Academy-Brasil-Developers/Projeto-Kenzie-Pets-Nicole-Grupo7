3aaded1aef8758312659cb2cae16529929b7f843
lucas <lcardoso662@gmail.com>
2022-11-09 11:36:08 -0400

    feat/updateprofile

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -2,8 +2,11 @@ import { openMenuNav, openCreatePetModal } from "../../scripts/openModal.js";
 import { renderAllMyPets } from "../../scripts/render.js";
 import { logout } from "../../scripts/logout.js";
 import { openMenuNavProfile } from "../../scripts/openModalProfile.js";
+import { modalUpdateProfile } from "../../scripts/openModalProfile.js";
 import { renderProfileUser } from "../../scripts/render.js";
 import { getUserTokenLocalStorage } from "../../scripts/localStorage.js";
+import { modalDeleteProfile } from "../../scripts/openModalProfile.js";
+import { moveToHome } from "../../scripts/render.js";
 
 const token = getUserTokenLocalStorage();
 
@@ -12,4 +15,7 @@ openCreatePetModal(token);
 renderAllMyPets(token);
 openMenuNavProfile();
 renderProfileUser();
+modalUpdateProfile()
 logout();
+modalDeleteProfile()
+moveToHome()
--- src/scripts/render.js
+++ src/scripts/render.js
@@ -44,7 +44,7 @@ export async function renderAllMyPets(token) {
 
 import { requestReadProfile } from "./requests.js";
 
-const { token } = getUserTokenLocalStorage();
+const token  = getUserTokenLocalStorage();
 //============== render profile ===================
 export async function renderProfileUser() {
   const request = await requestReadProfile(token);
@@ -56,4 +56,13 @@ export async function renderProfileUser() {
   avatarUser.src = `${request.avatar_url}`;
   nameUser.innerHTML = `<span>nome: </span> ${request.name}`;
   emailUser.innerHTML = `<span>email: </span> ${request.email}`;
+}
+
+//============== move to home ===================
+
+export function moveToHome (){
+  const button = document.querySelector('#btnToHome')
+  button.addEventListener('click',()=>{
+    window.location.replace('/src/pages/dashboard/index.html')
+  })
 }
